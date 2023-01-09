900d25350ad8c4d8b2a0cf582ba51425626f387f
Allan Pinheiro <allan.hal@gmail.com>
2023-01-08 21:22:19 -0300

    atualizando arquivos de rotas

--- routes/livroRoutes.js
+++ routes/livroRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const LivroController = require('../controllers/LivroController')
+const express = require("express");
+const LivroController = require("../controllers/livroController");
 
-const routes = express.Router()
+const routes = express.Router();
 //nao colocar "; porque pode encerrar o codigo.
 
 routes
-.get('/livros', LivroController.findAll)
-.get('/livro/:id', LivroController.findById)
-.post('/livro', LivroController.createBook)
-.delete('/livro/:id', LivroController.deleteBook)
-.put('/livro/:id', LivroController.updateBook)
+  .get("/livros", LivroController.findAll)
+  .get("/livro/:id", LivroController.findById)
+  .post("/livro", LivroController.createBook)
+  .delete("/livro/:id", LivroController.deleteBook)
+  .put("/livro/:id", LivroController.updateBook);
 
-module.exports = routes
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,35 +1,23 @@
 const express = require("express");
-// const routes = require('./routes/index')
+const routes = require("./routes/index");
 // create new express app and save it as "app"
 const app = express();
 
-// const routes = require('./routes/livroRoutes')
+const db = require("./config/db");
 
-// const db = require('./config/db')
-// // const livros = require('./models/livro')
-
-// db.on('error', console.log.bind(console, 'Erro ao conectar o banco!'))
-
-// db.once('open', () => {
-//   console.log('Conexao realizada com sucesso!')
-// })
-// // server configuration
+db.on("error", console.log.bind(console, "Erro ao conectar o banco!"));
+db.once("open", () => {
+  console.log("Conexao realizada com sucesso!");
+});
+// server configuration
 const PORT = 10000;
 
-// routes(app)
+routes(app);
 
 // create a route for the app
 app.get("/", (req, res) => {
   res.send("Servidor Subiu");
 });
-
-//mongoosse queries Ex: '.find'
-// app.get('/livros', (req, res) => {
-//   livros.find((error, livros) => {
-//     res.status(200).json(livros)
-//   })
-
-// })
 
 // make the server listen to requests
 app.listen(PORT, () => {
